bd183718ba851350f9a8036cd2cc76e03ead0a1f
Aditya Sharma <114949288+adityaaa009@users.noreply.github.com>
2025-08-11 11:28:55 +0530

    Updating for render

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 var methodOverride = require('method-override')
